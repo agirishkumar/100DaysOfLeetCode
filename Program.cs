d489c14745e19a38a74d9ed2261c608df705ee4e
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2021-01-10 22:26:09 +0530

    hello from girish

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace _100DaysOfLeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from Girish!");
         }
     }
 }
