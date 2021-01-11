c30f7443c514d7f9e740d92815b4ef12248f4426
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2021-01-11 12:35:26 +0530

    Two Sum with Loop

---
+++ TwoSum.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _100DaysOfLeetCode
+{
+    class TwoSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi" );
+        }
+    }
+}
